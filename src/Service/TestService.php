8b8c6e7e26497831795358b09ac4b73be87be6e2
Juan Ardissone (micayael) <micayael@gmail.com>
2022-02-19 16:01:15 -0300

    Se imprime mensaje de prueba

--- src/Service/TestService.php
+++ src/Service/TestService.php
@@ -4,14 +4,8 @@ namespace Micayael\Bundle\BaseBundle\Service;
 
 class TestService
 {
-    /** @required */
-    public string $kernelProjectDir;
-
-    /** @required */
-    public array $bundleConfig;
-
     public function test()
     {
-        dd($this->kernelProjectDir, $this->bundleConfig);
+        dd('test');
     }
 }
